ef3293d200b4828288699107d077fcf38257d33b
Wout's Mac <wout.van.de.sompele@student.howest.be>
2022-01-28 07:40:33 +0100

    added extra code to debug

--- src/js/script.js
+++ src/js/script.js
@@ -56,3 +56,4 @@ const initScrollTrigger = () => {
       ease: Power4.easeOut
     },
   });
+};
--- src/model/ShipList.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Illuminate\Database\Eloquent\Model;
-
-class ShipList extends Model {
-  // refer to a database table, an object us used here for demo purposes
-  public $timestamps = false;
-
-}
