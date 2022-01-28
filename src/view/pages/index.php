5cbcbda1d269d2666b8da62d54b21c2b36f9c224
Wout's Mac <wout.van.de.sompele@student.howest.be>
2022-01-28 07:24:52 +0100

    added more html

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,63 +1,71 @@
 <header class="header">
-
+  <div class="header__nav"><a class="header__link"><img class="header__slanted" src="./assets/img/slanted.png"></img></a></div>
 </header>
 
 <main class="main">
   <section class="intro">
     <div class="intro__title">
-      <p><span>M</span>argherita</p>
-      <p>Dall'</p>
-      <p><span class="pink">A</span>glio</p>
-    </div>
-    <div class="intro__text">
-      <p>The woman who continued where Bodoni couldn't</p>
+      <img class="intro__img" src="./assets/img/header_mobile.svg"></img>
     </div>
   </section>
 
   <section class="tragedy">
     <div class="tragedy__title">
-      <p>A tragedy has</p>
+      <p>A tragedy has <span>struck</span></p><!--<span class="struck--s">s</span>
+        <span class="struck--t">t</span>
+        <span class="struck--r">r</span>
+        <span class="struck--u">u</span>
+        <span class="struck--c">c</span>
+        <span class="struck--k">k</span></p>-->
       <div class="tragedy__title--struck">
-        <p class="struck--s">s</p>
-        <p class="struck--t">t</p>
-        <p class="struck--r">r</p>
-        <p class="struck--u">u</p>
-        <p class="struck--c">c</p>
-        <p class="struck--k">k</p>
+
       </div>
     </div>
     <div class="tragedy__text">
       <p>
-        In 1788 Giambattista Bodoni made published one of his biggest books ever. The <span>Manuale Tipografico</span>. It represents one of history's greatest typographical achievements. Bodoni has gotten a lot of praise for it at his time. It was made in order of the Duke of Parma. He as well showed its praise for the book: in 1789,  the Duke stated “well-designed type derived its beauty from four principles: uniformity of design, sharpness and neatness, good taste, and charm. All of which come together in La Manuale Tipografico.”
-        Shortly after the release of the first book, Bodoni decided to make a second edition, showing his new aquired skills and printing an even more impressive collection of typefaces.
+        <?php/*
+        foreach($texts as $text) {
+          echo $texts->$text;
+        }*/
 
-        But on November 30th 1813 a tragedy struck: Giambattista Bodoni <span>died</span>, leaving the second edition of the Manuale Tipografico unfinished. A great piece of history, never to be published. This would be an absolute tragedy for the world of typographers.
+
+        /*foreach($texts as $text) {
+          if($texts["part"] == "tragedy") {
+            echo $texts->text;
+          } else {
+            echo "no";
+          }
+          }*/;?>
       </p>
     </div>
   </section>
 
 <section class="unthinkable">
-    <h2 class="unthinkable__title">Margherita does the un <span>____</span> thinkable</h2>
+    <div class="unthinkable__reveal">
+      <img class="reveal__margherita" src="./assets/img/margherita.png"></img>
+      <img class="reveal__layer1" src="./assets/img/layer1.png"></img>
+      <img class="reveal__layer2" src="./assets/img/layer2.png"></img>
+    </div>
+    <h2 class="unthinkable__title">Margherita does the<br> un <span>____</span> thinkable</h2>
     <div class="unthinkable__text">
-      <p>Five years after the events all hope for the Manuale was lost. Until, in 1818 Margherita Dall’ Aglio, the widow of Bodoni made a miracle happened. Margherita had managed to complete the entire book, with the help of her assistant Luigi Orsi.
+      <p>Five years after the events all hope for the Manuale was lost. Until, in 1818 Margherita Dall’ Aglio, the widow of Bodoni made a miracle happened. Margherita had managed to complete the entire book, with the help of her assistant Luigi Orsi.<br><br>
 
-      The second edition was published in two volumes, it was over 600 pages long and contained 265 pages of roman characters, “imperceptibly declining in size, romans, italics, and script types, and the series of 125 capital letters; 181 pages of Greek and Oriental characters; 1036 decorations and 31 borders; followed in the last 20 pages by symbols, ciphers, numerals, and musical examples.
+      The second edition was published in two volumes, it was over 600 pages long and contained 265 pages of roman characters, “imperceptibly declining in size, romans, italics, and script types, and the series of 125 capital letters; 181 pages of Greek and Oriental characters; 1036 decorations and 31 borders; followed in the last 20 pages by symbols, ciphers, numerals, and musical examples.<br><br>
+
+      And it is to her that we owe the publication in 1818, of the Manuale Tipografico of the Cavaliere Giambattista Bodoni, the typographer's immense effort that lasted a lifetime and was realized only thanks to this woman who "knew well the gravity of the load I had to carry; but I gathered all my strength; love for him and for his glory sustained them; and I bravely set out on the task, so that Italy and Europe would not be deprived of such a distinguished monument of typography".<br><br>
 
       An absolute masterpiece in the world of typography. It was very impressive that the widow of Bodoni had so much skill to make this possible. Some were suprised, some may even have been sceptical of how this was possible. The question arrised: Who is Margherita Bodoni?
-
-      And it is to her that we owe the publication in 1818, of the Manuale Tipografico of the Cavaliere Giambattista Bodoni, the typographer's immense effort that lasted a lifetime and was realized only thanks to this woman who "knew well the gravity of the load I had to carry; but I gathered all my strength; love for him and for his glory sustained them; and I bravely set out on the task, so that Italy and Europe would not be deprived of such a distinguished monument of typography".</p>
+</p>
     </div>
-    <img class="unthinkable__margherita" src="./assets/planets/" height="256"></img>
-    <img class="unthinkable__layer1" src="./assets/planets/" height="256"></img>
-    <img class="unthinkable__layer2" src="./assets/planets/" height="256"></img>
   </section>
 
+  <div class="black">
   <section class="italy">
     <h2 class="italy__title">Where <span>lovers</span> meet</h2>
     <p class="italy__text">
-      Bodoni was born in Piedmont, Saluzzo. His father and grandfather were both printers in Piedmont. At the age of 17 he travelled to Rome where he mastered the skill of printing. After five years of learning he planned to move to England, which typography then florished under Baskerville's influence. But unfortunatly that year he succumbed to Tertian fever (Malaria). This lead him back to Saluzzo.
+      Bodoni was born in Piedmont, Saluzzo. His father and grandfather were both printers in Piedmont. At the age of 17 he travelled to Rome where he mastered the skill of printing. After five years of learning he planned to move to England, which typography then florished under Baskerville's influence. But unfortunatly that year he succumbed to Tertian fever (Malaria). This lead him back to Saluzzo.<br><br>
 
-      When the Duke of Parma made plans to open a Royal Press in Parma, his father recommended him. He got accepted, therefore Parma was now his new residence. This is where Margherita and Bodoni met. Margherita, who was born in Parma, never left the city since she met her husband there, and also worked for the printing house.
+      When the Duke of Parma made plans to open a Royal Press in Parma, his father recommended him. He got accepted, therefore Parma was now his new residence. This is where Margherita and Bodoni met. Margherita, who was born in Parma, never left the city since she met her husband there, and also worked for the printing house.<br><br>
 
       1791 was a special year for Bodoni who "in this year ... established the time of his greatest typographical fame, and the increase of his fortune, so that from the same year he repeated with the greatest pleasure in his domestic happiness." Margherita married Bodoni and was "his loving and inconsolable wife who, for twenty-two years, enlightened his labors with her lively, equable, and sweet character, beautifying his life, and prolonging its course by the most zealous and tender care."
     </p>
@@ -65,40 +73,42 @@
 
   <section class="righthand">
     <div class="righthand__title">
-      <p>B</p>
-      <p><span>odoni</span>'s</p>
-      <p>right</p>
-      <p>hand</p>
+      <img src="./assets/img/B.svg" alt="B" class="title__B">
+      <div class="title__words">
+        <h2><span>odoni</span>'s</h2>
+        <h2>right</h2>
+        <h2>hand</h2>
+      </div>
     </div>
     <p class="righthand__text">
-      Bodoni was born in Piedmont, Saluzzo. His father and grandfather were both printers in Piedmont. At the age of 17 he travelled to Rome where he mastered the skill of printing. After five years of learning he planned to move to England, which typography then florished under Baskerville's influence. But unfortunatly that year he succumbed to Tertian fever (Malaria). This lead him back to Saluzzo.
-
-      When the Duke of Parma made plans to open a Royal Press in Parma, his father recommended him. He got accepted, therefore Parma was now his new residence. This is where Margherita and Bodoni met. Margherita, who was born in Parma, never left the city since she met her husband there, and also worked for the printing house.
-
-      1791 was a special year for Bodoni who "in this year ... established the time of his greatest typographical fame, and the increase of his fortune, so that from the same year he repeated with the greatest pleasure in his domestic happiness." Margherita married Bodoni and was "his loving and inconsolable wife who, for twenty-two years, enlightened his labors with her lively, equable, and sweet character, beautifying his life, and prolonging its course by the most zealous and tender care."
+      Margherita was always by the side of the typographer Bodoni in a discreet way, but with ingenuity and skill at the same time. He accompanied him both personally and professionally, also when Bodoni had to deal with government figures. She spoke Italian as her mother tongue, but learned French and was able to read and understand English. Her husband fell ill and she had to be in charge, as secretary and adviser - in fact, as a businesswoman - of the correspondence that the firm maintained with personalities from all over Europe. She spoke Italian as her mother tongue, but learned French and was able to read and understand English.
     </p>
   </section>
+  </div>
 
   <section class="shadow">
     <h2 class="shadow__title">In the shadow of her man</h2>
     <p class="shadow__text">
-      Because Margheritta met Bodoni, she has had some great opportunities to work for the Royal Press. Because of Bodoni she was able to refine here typographic and printing skills. She was able to discover numerous of beautiful cities where Bodoni went. Although she was probably very greatful for that, their is also the reverse of the medal.
+      Because Margheritta met Bodoni, she has had some great opportunities to work for the Royal Press. Because of Bodoni she was able to refine here typographic and printing skills. She was able to discover numerous of beautiful cities where Bodoni went. Although she was probably very greatful for that, their is also the reverse of the medal.<br><br>
 
-      Although that we can see that she has oustanding skill in printing and typography, she will always be remembered as the widow of Bodoni. The wife that finished the book of Bodoni.
+      Although that we can see that she has oustanding skill in printing and typography, she will always be remembered as the widow of Bodoni. The wife that finished the book of Bodoni.<br><br>
 
-      She isn't the first women that has to suffer this fate. From the 15th century to the beginning of the 19th century, the printing and publishing of books was a family business, so women and daughters developed the basic skills to continue the work of their husbands or parents. That is why most women who have excelled in the world of publishing or typography are known as "the widow of ...", forgetting, as in many other moments in history and facets of art, their own work, talent and contribution they have made to history.
+      She isn't the first women that has to suffer this fate. From the 15th century to the beginning of the 19th century, the printing and publishing of books was a family business, so women and daughters developed the basic skills to continue the work of their husbands or parents. That is why most women who have excelled in the world of publishing or typography are known as "the widow of ...", forgetting, as in many other moments in history and facets of art, their own work, talent and contribution they have made to history.<br><br>
 
       But during the nineteenth century there were a number of radical changes resulting from the Industrial Revolution, which had an impact on the consideration of the space they occupied and the work that women did within the production process. The business ceased to be familiar and business structures changed. The printing presses were led by men, and women were diverted to other areas of the process, such as illustration and binding.
     </p>
   </section>
 
-  <section class="uncertainty"></section>
+  <section class="uncertainty">
+    <div class="uncertainty__content">
     <h2 class="uncertainty__title">Bodoni's painful uncertainty</h2>
     <p class="uncertainty__text">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo, felis ac volutpat bibendum, elit orci consequat risus, ac lacinia purus libero et lorem. In venenatis consequat diam eget efficitur. Nullam sed magna eget arcu ultrices pharetra quis vel velit.
+      An acute illness prevented Bodoni from fulfilling his work. On his death bed he didn't quite trust Margerita with the task of completing the work which represented a tremendous commitment to his fame and was of great importance to the profession.<br><br>
 
-      Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus tempus orci non auctor ullamcorper.
+      Perhaps he felt that such an undertaking was far beyond my ability. It is true that before that time I had feared the extreme difficulties of composing and arranging even the last parts of so enormous a work.
     </p>
+    </div>
+  </section>
 
   <section class="hidden">
     <h2 class="hidden__title">Margheritta's hidden talents</h2>
@@ -117,7 +127,7 @@
       <p>a Bodoni?</p>
     </div>
     <p class="defines__intro">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo, felis ac volutpat bibendum, elit orci consequat risus, ac lacinia purus libero et lorem. In venenatis consequat diam eget efficitur. Nullam sed magna eget arcu ultrices.
+      Margherita his printed dozens of books in her husband's typeface. But what makes this font so special, where his widow spent another life time perfecting it. Find out about the characteristics of this immortal font, that in this time, is still being used frequently.
     </p>
   </section>
 
@@ -133,91 +143,16 @@
 </main>
 
 <footer class="footer">
-  <img class="footer__slanted" src="./assets/planets/"></img>
+  <img class="footer__slanted" src="./assets/img/SLANTED_SMOL.png"></img>
   <p class="footer__text">Did this article about Giambattista Bodoni and his wife scratch your graphic design itch? This is a lot more discover in the world of graphic and contemporary art. Two times a year Slanted publishes a magazine containing some of the best artistic works form all around the globe. All magazines handle a general theme, the first books focus on a specific world changing city. The current magazines focus on colors, shapes and technological advancements in our sector. Slanted inspires.</p>
-  <div class="footer__magazine"><img class="magazine__picture" src="./assets/planets/"></img><h3 class="magazine__title">CHECK OUT OUR LATEST MAGAZINE</h3><p class="magazine__text">The issue celebrates happiness, joy of life, power, symbolism, and the meaning of color. We look...</p></div>
+  <div class="footer__magazine"><img class="magazine__picture" src="./assets/img/slanted_book.jpg" height=222px></img>
+  <div class="magazine__content">
+  <h3 class="magazine__title">CHECK OUT OUR<br> LATEST MAGAZINE</h3><p class="magazine__text">The issue celebrates happiness, joy of life, power, symbolism, and the meaning of color. We look...</p></div></div>
   <div class="footer__socials">
-  <img class="socials__twitter" src="./assets/planets/"></img>
-  <img class="socials__facebook" src="./assets/planets/"></img>
-  <img class="socials__instagram" src="./assets/planets/"></img>
-  <img class="socials__youtube" src="./assets/planets/"></img>
+  <img class="socials__twitter" src="./assets/img/twitter.svg"></img>
+  <img class="socials__facebook" src="./assets/img/facebook.svg"></img>
+  <img class="socials__instagram" src="./assets/img/instagram.svg"></img>
+  <img class="socials__youtube" src="./assets/img/youtube.svg"></img>
   </div>
   <p class="footer__license">&copy; slanted publishers 2022</p>
 </footer>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<section class="content">
-<article class="login__content">
-<div class="login__logo__container"><img class="login__logo" src="./assets/img/logo.png" alt="Logo rebel planner" width="50px" height="50px"></img></div>
-<div class="login__title__container"><h class="login__title">Rebel Alliance Mission Planner</h></div>
-<div class="login__container">
-  <h2 class="login__subtitle">Login to access the rebel alliance planner</h2>
-    <form method="post" action="index.php" class="form">
-        <input type="hidden" name="action" value="userLogin">
-
-        <label class="form__login__title">Username</label>
-
-          <div class="login__input__container"><input class="login__input <?php
-          if(!empty($_POST) && empty($_POST['username'])) {
-            echo 'redborder';
-          }
-          ?>" type="text" name="username" placeholder="Username" value=""></div>
-
-        <?php
-          if(!empty($_POST) && empty($_POST['username'])) {
-            echo '<p class="error__login">Please enter in a username</p>';
-          }
-          ?>
-
-        <!----------------------->
-
-          <label class="form__login__title">Password</label>
-
-          <div class="login__input__container"><input class="login__input <?php
-          if(!empty($wrongPassword)) {
-            echo "redborder";
-          }?>" type="password" name="password" placeholder="Password" value=""></div>
-
-        <?php
-          if(!empty($wrongPassword)) {
-            echo '<p class="error__login">Wrong password</p>';
-          }
-          ?>
-
-          <div class="login__submit__container"><input class="login__submit__button" type="submit" value="Log in"></div>
-    </form>
-<div class="login__disclaimer__container">
-<p class="login__disclaimer__text">[DISCLAIMER: only for people within the rebel alliance]</p>
-<p class="login__disclaimer__text">KEEP THE PLANNER SECRET</p>
-</div>
-</div>
-</article>
-</section>
