5ed49fd029767b66ddba59941a7cdaacafb7f5bf
Wout's Mac <wout.van.de.sompele@student.howest.be>
2022-01-28 07:26:32 +0100

    text comes from database closes #23

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -11,7 +11,7 @@
 
   <section class="tragedy">
     <div class="tragedy__title">
-      <p>A tragedy has <span>struck</span></p><!--<span class="struck--s">s</span>
+      <p><?php echo $tragedy_title ?></p><!--<span class="struck--s">s</span>
         <span class="struck--t">t</span>
         <span class="struck--r">r</span>
         <span class="struck--u">u</span>
@@ -23,19 +23,9 @@
     </div>
     <div class="tragedy__text">
       <p>
-        <?php/*
-        foreach($texts as $text) {
-          echo $texts->$text;
-        }*/
-
-
-        /*foreach($texts as $text) {
-          if($texts["part"] == "tragedy") {
-            echo $texts->text;
-          } else {
-            echo "no";
-          }
-          }*/;?>
+        <?php
+          echo $tragedy;
+        ?>
       </p>
     </div>
   </section>
@@ -46,92 +36,92 @@
       <img class="reveal__layer1" src="./assets/img/layer1.png"></img>
       <img class="reveal__layer2" src="./assets/img/layer2.png"></img>
     </div>
-    <h2 class="unthinkable__title">Margherita does the<br> un <span>____</span> thinkable</h2>
+    <h2 class="unthinkable__title"><?php echo $unthinkable_title ?></h2>
     <div class="unthinkable__text">
-      <p>Five years after the events all hope for the Manuale was lost. Until, in 1818 Margherita Dall’ Aglio, the widow of Bodoni made a miracle happened. Margherita had managed to complete the entire book, with the help of her assistant Luigi Orsi.<br><br>
-
-      The second edition was published in two volumes, it was over 600 pages long and contained 265 pages of roman characters, “imperceptibly declining in size, romans, italics, and script types, and the series of 125 capital letters; 181 pages of Greek and Oriental characters; 1036 decorations and 31 borders; followed in the last 20 pages by symbols, ciphers, numerals, and musical examples.<br><br>
-
-      And it is to her that we owe the publication in 1818, of the Manuale Tipografico of the Cavaliere Giambattista Bodoni, the typographer's immense effort that lasted a lifetime and was realized only thanks to this woman who "knew well the gravity of the load I had to carry; but I gathered all my strength; love for him and for his glory sustained them; and I bravely set out on the task, so that Italy and Europe would not be deprived of such a distinguished monument of typography".<br><br>
-
-      An absolute masterpiece in the world of typography. It was very impressive that the widow of Bodoni had so much skill to make this possible. Some were suprised, some may even have been sceptical of how this was possible. The question arrised: Who is Margherita Bodoni?
-</p>
+      <p>
+        <?php
+          echo $unthinkable;
+        ?>
+      </p>
     </div>
   </section>
 
   <div class="black">
   <section class="italy">
-    <h2 class="italy__title">Where <span>lovers</span> meet</h2>
+    <h2 class="italy__title"><?php echo $italy_title ?></h2>
     <p class="italy__text">
-      Bodoni was born in Piedmont, Saluzzo. His father and grandfather were both printers in Piedmont. At the age of 17 he travelled to Rome where he mastered the skill of printing. After five years of learning he planned to move to England, which typography then florished under Baskerville's influence. But unfortunatly that year he succumbed to Tertian fever (Malaria). This lead him back to Saluzzo.<br><br>
-
-      When the Duke of Parma made plans to open a Royal Press in Parma, his father recommended him. He got accepted, therefore Parma was now his new residence. This is where Margherita and Bodoni met. Margherita, who was born in Parma, never left the city since she met her husband there, and also worked for the printing house.<br><br>
-
-      1791 was a special year for Bodoni who "in this year ... established the time of his greatest typographical fame, and the increase of his fortune, so that from the same year he repeated with the greatest pleasure in his domestic happiness." Margherita married Bodoni and was "his loving and inconsolable wife who, for twenty-two years, enlightened his labors with her lively, equable, and sweet character, beautifying his life, and prolonging its course by the most zealous and tender care."
+        <?php
+          echo $italy;
+        ?>
     </p>
   </section>
+
 
   <section class="righthand">
     <div class="righthand__title">
       <img src="./assets/img/B.svg" alt="B" class="title__B">
       <div class="title__words">
+        <br><br>
         <h2><span>odoni</span>'s</h2>
         <h2>right</h2>
         <h2>hand</h2>
       </div>
     </div>
     <p class="righthand__text">
-      Margherita was always by the side of the typographer Bodoni in a discreet way, but with ingenuity and skill at the same time. He accompanied him both personally and professionally, also when Bodoni had to deal with government figures. She spoke Italian as her mother tongue, but learned French and was able to read and understand English. Her husband fell ill and she had to be in charge, as secretary and adviser - in fact, as a businesswoman - of the correspondence that the firm maintained with personalities from all over Europe. She spoke Italian as her mother tongue, but learned French and was able to read and understand English.
+        <?php
+          echo $righthand;
+        ?>
     </p>
   </section>
   </div>
 
   <section class="shadow">
-    <h2 class="shadow__title">In the shadow of her man</h2>
+    <div class="shadow__title--wrapper"><img src="./assets/img/shadow.svg" alt="In the shadow of her man" class="shadow__title"></div>
     <p class="shadow__text">
-      Because Margheritta met Bodoni, she has had some great opportunities to work for the Royal Press. Because of Bodoni she was able to refine here typographic and printing skills. She was able to discover numerous of beautiful cities where Bodoni went. Although she was probably very greatful for that, their is also the reverse of the medal.<br><br>
-
-      Although that we can see that she has oustanding skill in printing and typography, she will always be remembered as the widow of Bodoni. The wife that finished the book of Bodoni.<br><br>
-
-      She isn't the first women that has to suffer this fate. From the 15th century to the beginning of the 19th century, the printing and publishing of books was a family business, so women and daughters developed the basic skills to continue the work of their husbands or parents. That is why most women who have excelled in the world of publishing or typography are known as "the widow of ...", forgetting, as in many other moments in history and facets of art, their own work, talent and contribution they have made to history.<br><br>
-
-      But during the nineteenth century there were a number of radical changes resulting from the Industrial Revolution, which had an impact on the consideration of the space they occupied and the work that women did within the production process. The business ceased to be familiar and business structures changed. The printing presses were led by men, and women were diverted to other areas of the process, such as illustration and binding.
+        <?php
+          echo $shadow;
+        ?>
     </p>
   </section>
 
   <section class="uncertainty">
     <div class="uncertainty__content">
-    <h2 class="uncertainty__title">Bodoni's painful uncertainty</h2>
-    <p class="uncertainty__text">
-      An acute illness prevented Bodoni from fulfilling his work. On his death bed he didn't quite trust Margerita with the task of completing the work which represented a tremendous commitment to his fame and was of great importance to the profession.<br><br>
-
-      Perhaps he felt that such an undertaking was far beyond my ability. It is true that before that time I had feared the extreme difficulties of composing and arranging even the last parts of so enormous a work.
-    </p>
+    <h2 class="uncertainty__title"><?php echo $uncertainty_title ?></h2>
+    <div class="uncertainty__text">
+      <p>
+        <?php
+          echo $uncertainty;
+        ?>
+      </p>
+    </div>
     </div>
   </section>
 
-  <section class="hidden">
-    <h2 class="hidden__title">Margheritta's hidden talents</h2>
-    <div  class="hidden__text1">
-      Passionate about music, she was elected a regular member of the Ducal Parmens Philharmonic Society on April 2, 1825.
-    </div>
-    <div  class="hidden__text2">
-      She became member of Accademia degli Unanimi , of which she was made an honorary member on 26/12/1796, with the name "L'Eccelsa."
+  <section class="talents">
+    <h2 class="talents__title"><?php echo $talents_title ?></h2>
+    <div class="talents__reveal">
+      <div class="questionmark__wrapper"><img class="talents__questionmark" src="./assets/img/questionmark.svg"></img></div>
+      <div class="talents__content">
+        <div  class="talents__text1">
+          Passionate about music, she was elected a regular member of the Ducal Parmens Philharmonic Society on April 2, 1825.
+        </div>
+        <div  class="talents__text2">
+          She became member of Accademia degli Unanimi , of which she was made an honorary member on 26/12/1796, with the name "L'Eccelsa."
+        </div>
+      </div>
     </div>
   </section>
 
   <section class="defines">
-    <div class="defines__title">
-      <p>What</p>
-      <p>defines</p>
-      <p>a Bodoni?</p>
-    </div>
+    <img class="defines__title" src="./assets/img/defines_mobile.svg"></img>
     <p class="defines__intro">
-      Margherita his printed dozens of books in her husband's typeface. But what makes this font so special, where his widow spent another life time perfecting it. Find out about the characteristics of this immortal font, that in this time, is still being used frequently.
+        <?php
+          echo $defines;
+        ?>
     </p>
   </section>
 
-  <section class="characteristics"></section>
+
 
   <section class="end">
     <p>Thanks to</p>
@@ -144,7 +134,11 @@
 
 <footer class="footer">
   <img class="footer__slanted" src="./assets/img/SLANTED_SMOL.png"></img>
-  <p class="footer__text">Did this article about Giambattista Bodoni and his wife scratch your graphic design itch? This is a lot more discover in the world of graphic and contemporary art. Two times a year Slanted publishes a magazine containing some of the best artistic works form all around the globe. All magazines handle a general theme, the first books focus on a specific world changing city. The current magazines focus on colors, shapes and technological advancements in our sector. Slanted inspires.</p>
+  <p class="footer__text">
+        <?php
+          echo $footer;
+        ?>
+  </p>
   <div class="footer__magazine"><img class="magazine__picture" src="./assets/img/slanted_book.jpg" height=222px></img>
   <div class="magazine__content">
   <h3 class="magazine__title">CHECK OUT OUR<br> LATEST MAGAZINE</h3><p class="magazine__text">The issue celebrates happiness, joy of life, power, symbolism, and the meaning of color. We look...</p></div></div>
