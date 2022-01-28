1748662e5aceb29491991f00f8ea8e376b0bf712
Wout's Mac <wout.van.de.sompele@student.howest.be>
2022-01-28 07:14:44 +0100

    added Text model for #23

---
+++ src/model/Text.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Text extends Model {
+  // refer to a database table, an object us used here for demo purposes
+  public $timestamps = false;
+
+}
